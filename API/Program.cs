6f0f4ba347e13ec7444c2a0fe32a114f3463cdc2
Tobias Würth <tobiaswuerth@users.noreply.github.com>
2017-12-20 12:18:58 +0100

    created initial project

---
+++ API/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+
+namespace ch.wuerth.tobias.mux.API
+{
+    public class Program
+    {
+        public static void Main(String[] args)
+        {
+            BuildWebHost(args).Run();
+        }
+
+        public static IWebHost BuildWebHost(String[] args)
+        {
+            return WebHost.CreateDefaultBuilder(args).UseStartup<Startup>().Build();
+        }
+    }
+}
