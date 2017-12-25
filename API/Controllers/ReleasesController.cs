692955e49db205ccd6b9701c20ad704844762359
Tobias Würth <tobiaswuerth@users.noreply.github.com>
2017-12-25 10:00:55 +0100

    added search for releases

--- API/Controllers/ReleasesController.cs
+++ API/Controllers/ReleasesController.cs
@@ -145,5 +145,39 @@ namespace ch.wuerth.tobias.mux.API.Controllers
                 return HandleException(ex);
             }
         }
+
+        [HttpGet("auth/releases/search/{query}")]
+        public IActionResult GetBySearchQuery(String query, [FromQuery(Name = "ps")] Int32 pageSize = 50,
+            [FromQuery(Name = "p")] Int32 page = 0)
+        {
+            try
+            {
+                if (!IsAuthorized(out IActionResult result))
+                {
+                    return result;
+                }
+
+                // validate
+                if (String.IsNullOrWhiteSpace(query))
+                {
+                    return StatusCode((Int32) HttpStatusCode.BadRequest);
+                }
+
+                NormalizePageSize(ref pageSize);
+                query = query.Trim();
+
+                // get data
+                using (DataContext dc = NewDataContext())
+                {
+                    return Ok(dc.SetReleases.AsNoTracking().Where(x => x.Title.Contains(query)).Select(x => x.Title)
+                        .OrderBy(x => x).Distinct().Skip(page * pageSize).Take(pageSize)
+                        .Select(x => new Dictionary<String, Object> {{"Title", x}}).ToList());
+                }
+            }
+            catch (Exception ex)
+            {
+                return HandleException(ex);
+            }
+        }
     }
 }
