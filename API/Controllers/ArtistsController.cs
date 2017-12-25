e22caa963b5b6c4dec1d7e80adfb3b6316441528
Tobias Würth <tobiaswuerth@users.noreply.github.com>
2017-12-25 19:35:33 +0100

    added artists controller

---
+++ API/Controllers/ArtistsController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+
+namespace ch.wuerth.tobias.mux.API.Controllers
+{
+    public class ArtistsController : DataController
+    {
+        public ArtistsController(IConfiguration configuration) : base(configuration) { }
+    }
+}
