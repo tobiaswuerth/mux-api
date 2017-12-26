9d7b1556e3042efcca08b85a2fae5a30f4105fe6
Tobias Würth <tobiaswuerth@users.noreply.github.com>
2017-12-26 18:47:45 +0100

    added api endpoint to get track by id

--- API/Controllers/TracksController.cs
+++ API/Controllers/TracksController.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Linq;
+using System.Net;
 using ch.wuerth.tobias.mux.API.extensions;
 using ch.wuerth.tobias.mux.Data;
+using ch.wuerth.tobias.mux.Data.models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
@@ -30,6 +32,42 @@ namespace ch.wuerth.tobias.mux.API.Controllers
                 {
                     return Ok(dc.SetTracks.AsNoTracking().Skip(page * pageSize).Take(pageSize)
                         .Select(x => x.ToJsonDictionary()).ToList());
+                }
+            }
+            catch (Exception ex)
+            {
+                return HandleException(ex);
+            }
+        }
+
+        [HttpGet("auth/tracks/{id}")]
+        public IActionResult GetById(Int32? id)
+        {
+            try
+            {
+                if (!IsAuthorized(out IActionResult result))
+                {
+                    return result;
+                }
+
+                // validate
+                if (id == null)
+                {
+                    return StatusCode((Int32) HttpStatusCode.BadRequest);
+                }
+
+                // get data
+                using (DataContext dc = NewDataContext())
+                {
+                    Track track = dc.SetTracks.AsNoTracking().FirstOrDefault(x => x.UniqueId.Equals(id));
+
+                    if (null == track)
+                    {
+                        // no matching record found
+                        return StatusCode((Int32) HttpStatusCode.NotFound);
+                    }
+
+                    return Ok(track.ToJsonDictionary());
                 }
             }
             catch (Exception ex)
