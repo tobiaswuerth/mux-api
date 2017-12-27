c0cfcaa85e13ae414ddd59f03ebb365849e62ff6
Tobias Würth <tobiaswuerth@users.noreply.github.com>
2017-12-27 16:31:38 +0100

    added get possible records of tracks by id

--- API/Controllers/TracksController.cs
+++ API/Controllers/TracksController.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using ch.wuerth.tobias.mux.API.extensions;
@@ -68,6 +69,70 @@ namespace ch.wuerth.tobias.mux.API.Controllers
                     }
 
                     return Ok(track.ToJsonDictionary());
+                }
+            }
+            catch (Exception ex)
+            {
+                return HandleException(ex);
+            }
+        }
+
+        [HttpGet("auth/tracks/{id}/records")]
+        public IActionResult GetRecordsById(Int32? id)
+        {
+            try
+            {
+                if (!IsAuthorized(out IActionResult result))
+                {
+                    return result;
+                }
+
+                // validate
+                if (id == null)
+                {
+                    return StatusCode((Int32) HttpStatusCode.BadRequest);
+                }
+
+                // get data
+                using (DataContext dc = NewDataContext())
+                {
+                    Track track = dc.SetTracks.AsNoTracking().Include(x => x.AcoustIdResults)
+                        .ThenInclude(x => x.AcoustId).ThenInclude(x => x.MusicBrainzRecordAcoustIds)
+                        .ThenInclude(x => x.MusicBrainzRecord).FirstOrDefault(x => x.UniqueId.Equals(id));
+
+                    if (null == track)
+                    {
+                        // no matching record found
+                        return StatusCode((Int32) HttpStatusCode.NotFound);
+                    }
+
+                    // todo might need optimization with a direct database sql query
+
+                    // loop references and keep track of relevant stats
+                    Dictionary<MusicBrainzRecord, (Int32 count, Double sum)> ret =
+                        new Dictionary<MusicBrainzRecord, (Int32, Double)>();
+
+                    track.AcoustIdResults.ForEach(x =>
+                    {
+                        Double score = x.Score;
+                        x.AcoustId.MusicBrainzRecordAcoustIds.ForEach(y =>
+                        {
+                            MusicBrainzRecord record = y.MusicBrainzRecord;
+                            if (!ret.ContainsKey(record))
+                            {
+                                ret.Add(record, (1, score));
+                                return; // continue
+                            }
+
+                            ret[record] = (ret[record].count + 1, ret[record].sum + score);
+                        });
+                    });
+
+                    return Ok(ret.Select(x => new Dictionary<String, Object>
+                    {
+                        {"Score", x.Value.sum / x.Value.count}, // calculate match score (average)
+                        {"Record", x.Key.ToJsonDictionary()}
+                    }));
                 }
             }
             catch (Exception ex)
