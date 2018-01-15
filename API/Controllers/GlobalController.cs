ff8356080d6ff8802582c609e6135f8e42fff99d
Tobias Würth <tobiaswuerth@users.noreply.github.com>
2018-01-15 06:52:01 +0100

    add auth/globals endpoint

--- API/Controllers/GlobalController.cs
+++ API/Controllers/GlobalController.cs
@@ -1,9 +1,57 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using ch.wuerth.tobias.mux.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 
 namespace ch.wuerth.tobias.mux.API.Controllers
 {
     public class GlobalController : DataController
     {
         public GlobalController(IConfiguration configuration) : base(configuration) { }
+
+        [ HttpGet("auth/globals") ]
+        public IActionResult Get()
+        {
+            try
+            {
+                if (!IsAuthorized(out IActionResult result))
+                {
+                    return result;
+                }
+
+                using (DataContext dataContext = NewDataContext())
+                {
+                    return Ok(new Dictionary<String, Object>
+                    {
+                        {
+                            "Counts", new Dictionary<String, Object>
+                            {
+                                {
+                                    "Tracks", dataContext.SetTracks.Count()
+                                }
+                                ,
+                                {
+                                    "Records", dataContext.SetMusicBrainzRecords.Count()
+                                }
+                                ,
+                                {
+                                    "Releases", dataContext.SetReleases.Count()
+                                }
+                                ,
+                                {
+                                    "Artists", dataContext.SetArtists.Count()
+                                }
+                            }
+                        }
+                    });
+                }
+            }
+            catch (Exception ex)
+            {
+                return HandleException(ex);
+            }
+        }
     }
 }
