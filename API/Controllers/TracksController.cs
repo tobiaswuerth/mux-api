9bac73f0058c6cd1f76b447c5ff314715b509d95
Tobias Würth <tobiaswuerth@users.noreply.github.com>
2018-01-16 06:52:01 +0100

    removed base class constructor

--- API/Controllers/TracksController.cs
+++ API/Controllers/TracksController.cs
@@ -7,14 +7,11 @@ using ch.wuerth.tobias.mux.Data;
 using ch.wuerth.tobias.mux.Data.models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
 
 namespace ch.wuerth.tobias.mux.API.Controllers
 {
     public class TracksController : DataController
     {
-        public TracksController(IConfiguration configuration) : base(configuration) { }
-
         [ HttpGet("auth/tracks") ]
         public IActionResult GetAll([ FromQuery(Name = "ps") ] Int32 pageSize = 50, [ FromQuery(Name = "p") ] Int32 page = 0)
         {
