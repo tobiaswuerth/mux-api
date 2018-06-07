a431b4dbed9a6789268f3a473954627b0bfb331b
Tobias Würth <tobiaswuerth@users.noreply.github.com>
2018-06-07 18:28:42 +0200

    add user controller

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using ch.wuerth.tobias.mux.Core.logging;
+using ch.wuerth.tobias.mux.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ch.wuerth.tobias.mux.API.Controllers
+{
+    public class UsersController : DataController
+    {
+        [ HttpGet("auth/users") ]
+        public IActionResult Get([ FromQuery(Name = "ps") ] Int32 pageSize = 50, [ FromQuery(Name = "p") ] Int32 page = 0)
+        {
+            try
+            {
+                LoggerBundle.Trace("Registered GET request on UsersController.Get");
+                if (!IsAuthorized(out IActionResult result))
+                {
+                    LoggerBundle.Trace("Request not authorized");
+                    return result;
+                }
+
+                NormalizePageSize(ref pageSize);
+
+                // get data
+                using (DataContext dc = DataContextFactory.GetInstance())
+                {
+                    return Ok(dc.SetUsers.AsNoTracking()
+                        .OrderBy(x => x.Username)
+                        .Skip(page * pageSize)
+                        .Take(pageSize)
+                        .Select(x => new Dictionary<String, Object>
+                        {
+                            {
+                                "UniqueId", x.UniqueId
+                            }
+                            ,
+                            {
+                                "Username", x.Username
+                            }
+                        })
+                        .ToList());
+                }
+            }
+            catch (Exception ex)
+            {
+                return HandleException(ex);
+            }
+        }
+    }
+}
